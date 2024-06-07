53b9fe570614e1f244ec8eaf845f4c26e7df7af5
Winnie Mutheu <mutheuw62@gmail.com>
2024-06-07 12:36:50 +0300

    This a tic tac toe game created in Python

---
+++ main.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/python3
+
+require_relative 'tic_tac_toe.py'
+
+# board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+# game = TicTacToe.new(board)
+game = TicTacToe.new
+game.display_board
+game.play
